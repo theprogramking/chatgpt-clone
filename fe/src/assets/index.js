8ac15babca4d3b9b67c1cf96f97114a6815ea356
Luke Shimkus <luke.shimkus@ad.merchantlynx.com>
2025-05-20 19:39:56 -0400

    UI IS GETTING THERE

---
+++ fe/src/assets/index.js
@@ -0,0 +1,9 @@
+export { default as EyeIcon } from './EyeIcon';
+export { default as PlusIcon } from './PlusIcon';
+export { default as LightBulbIcon } from './LightbulbIcon';
+export { default as TelescopeIcon } from './TelescopeIcon';
+export { default as ArtIcon } from './ArtIcon';
+export { default as DotsIcon } from './DotsIcon';
+export { default as MicrophoneIcon } from './MicrophoneIcon';
+export { default as UpArrowIcon } from './UpArrowIcon';
+export { default as SoundWaveIcon } from './SoundWaveIcon';
